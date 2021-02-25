a2d981750c07b4af17d88980742ab193d2b5f724
Heitor de Souza <heisou06@gmail.com>
2021-02-25 17:45:33 +0000

    created countdown context

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
+import { ChallengeBox } from './ChallengeBox';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext)
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/heitor92.png" alt="Heitor de Souza" />
@@ -8,7 +13,7 @@ export function Profile() {
                 <strong>Heitor de Souza</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
